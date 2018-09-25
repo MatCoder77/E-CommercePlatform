a5c154deedf2710fd9316c7a85a8170b5e4d6955
MatCoder77 <matkimzag@wp.pl>
2018-09-26 01:29:24 +0200

    Fixed wrong displaying incons, prapared menus for dynamic generation

--- ECommercePlatformConsole/src/main/java/ecommerceplatform/console/MainWindowController.java
+++ ECommercePlatformConsole/src/main/java/ecommerceplatform/console/MainWindowController.java
@@ -2,7 +2,9 @@ package ecommerceplatform.console;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
@@ -12,7 +14,13 @@ import com.e_commerce_platform.api.CategoryDTO;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 
+import de.jensd.fx.glyphs.GlyphIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import de.jensd.fx.glyphs.materialicons.MaterialIcon;
+import de.jensd.fx.glyphs.materialicons.MaterialIconView;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -20,13 +28,17 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import ecommerceplatform.console.client.ServerClient;
 
 public class MainWindowController implements Initializable{
 	
+	@Inject
+	private ServerClient serverClient;
 	
 	@FXML
 	private JFXButton closeButton;
@@ -46,28 +58,23 @@ public class MainWindowController implements Initializable{
     private JFXDrawer subcategoryDrower;
 	
 	@FXML
-    private JFXButton electronicButton;
-	
-	@FXML
 	private StackPane stackPane;
 	
 	@FXML
     private GridPane menu;
+	
+	private int rowCounter = 1;
 
+	private GlyphIcon<?> currentIcon;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		/*FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("main/resources/view/SubcategoryPane.fxml"));
-		loader.setController(new SubcategoryPaneController(this));
-		GridPane content = loader.load();//FXMLLoader.load(getClass().getResource("/SubcategoryPane.fxml"));*/
 		ServerClient client = new ServerClient();
 		List<CategoryDTO> categories = client.getMainCategories();
-		List<String> subcategories = categories.stream()
-				.map(CategoryDTO::getName)
-				.collect(Collectors.toList());
-		SubcategoryPane subcategoryPane = new SubcategoryPane(this, subcategories);
-		subcategoryDrower.setSidePane(subcategoryPane);
-		subcategoryDrower.getSidePane().get(0).toFront();	
+		creteMainMenu(categories);
+//		SubcategoryPane subcategoryPane = createSubcategoryMenu(categories.get(0).getSubcategories());
+//		subcategoryDrower.setSidePane(subcategoryPane);
+//		subcategoryDrower.getSidePane().get(0).toFront();	
 	}
 	@FXML
     void buttonClicked(ActionEvent event) {
@@ -85,12 +92,12 @@ public class MainWindowController implements Initializable{
 	@FXML
     void onDrawerClosed(Event event) {
 		Platform.runLater(() -> FXCollections.reverse(stackPane.getChildren()));
-		Platform.runLater(() -> electronicIcon.setFill(Color.WHITE));
+		Platform.runLater(() -> currentIcon.setFill(Color.WHITE));
     }
 
     @FXML
     void onDrawerOpended(Event event) {
-    	Platform.runLater(() -> electronicIcon.setFill(Color.web("#FFD80D")));
+    	Platform.runLater(() -> currentIcon.setFill(Color.web("#FFD80D")));
     }
     
     @FXML
@@ -100,6 +107,57 @@ public class MainWindowController implements Initializable{
     
     void hideSubcategoryPane() {
     	subcategoryDrower.close();
+    }
+    
+    void createSideMenu() {
+    	List<CategoryDTO> mainCategories = serverClient.getMainCategories();
+    	mainCategories.stream()
+    			.forEach(this::createCategory);
+    }
+    
+    void creteMainMenu(List<CategoryDTO> categories) {
+    	categories.stream()
+    			.forEach(this::createCategory);
+    }
+    
+    private void createCategory(CategoryDTO category) {
+    	JFXButton button = new JFXButton(category.getName());
+    	GlyphIcon<?> icon = getIcon(category.getIconName());
+    	menu.add(icon, 0, rowCounter);
+    	menu.add(button, 1, rowCounter++);
+    	RowConstraints rowConstraints = new RowConstraints(50,50, 50);
+    	menu.getRowConstraints().add(rowConstraints);
+    	SubcategoryPane subcategoryPane = createSubcategoryMenu(category.getSubcategories());
+    	subcategoryDrower.setSidePane(subcategoryPane);
+		button.setOnAction(event -> {
+			subcategoryDrower.setSidePane(subcategoryPane);
+			buttonClicked(event);
+			currentIcon = icon;
+		});
+    }
+    
+    private GlyphIcon<?> getIcon(String iconName) {
+    	String[] arr = iconName.split("\\.");
+    	switch (arr[0]) {
+		case "MDI":
+			return new MaterialDesignIconView(MaterialDesignIcon.valueOf(arr[1]));
+		case "MI":
+			return new MaterialIconView(MaterialIcon.valueOf(arr[1]));
+		case "FAI":
+			return new FontAwesomeIconView(FontAwesomeIcon.valueOf(arr[1]));
+		default:
+			throw new IllegalArgumentException("WRONG ICON NAME " + iconName);
+		}
+    }
+    
+    private SubcategoryPane createSubcategoryMenu(Collection<CategoryDTO> subcategories) {
+    	Map<Integer, String> subcategoriesMap = subcategories.stream()
+    			.collect(Collectors.toMap(CategoryDTO::getId, CategoryDTO::getName));
+    	return new SubcategoryPane(this, subcategoriesMap);
+    }
+    
+    public void choosenCategory(Integer categoryId) {
+    	System.out.println("Wybrano kategoriê o ID: " + categoryId);
     }
 
 }
--- ECommercePlatformConsole/src/main/java/ecommerceplatform/console/SubcategoryPane.java
+++ ECommercePlatformConsole/src/main/java/ecommerceplatform/console/SubcategoryPane.java
@@ -3,8 +3,12 @@ package ecommerceplatform.console;
 import java.awt.Button;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
+import com.e_commerce_platform.api.CategoryDTO;
 import com.jfoenix.controls.JFXButton;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.GridPane;
@@ -15,17 +19,18 @@ public class SubcategoryPane extends GridPane{
 	private JFXButton backButton;
 	
 	private int rowCounter = 1;
-	private List<String> subcategories;
+	private Map<Integer, String> subcategories;
+	private MainWindowController controller;
 	
-	public SubcategoryPane(MainWindowController controller, List<String> subcategories) {
-		this.subcategories = subcategories;		
+	public SubcategoryPane(MainWindowController controller, Map<Integer, String> subcategories) {
+		this.subcategories = subcategories;	
+		this.controller = controller;
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("main/resources/view/SubcategoryPane.fxml"));
 			loader.setController(new SubcategoryPaneController(controller));
 			loader.setRoot(this);
 			loader.load();
-			subcategories.stream()
-				.map(JFXButton::new)
+			subcategories.entrySet()
 				.forEach(this::addButton);
 			
 		} catch (IOException e) {
@@ -34,12 +39,14 @@ public class SubcategoryPane extends GridPane{
 		}
 	}
 	
-	private void addButton(JFXButton button) {
+	private void addButton(Map.Entry<Integer, String> subcategory) {
 		//button.setMaxHeight(Double.MAX_VALUE);
+		JFXButton button = new JFXButton(subcategory.getValue());
 		button.setMaxWidth(Double.MAX_VALUE);
 		RowConstraints rowConstraints = new RowConstraints(40,40,40);
 		getRowConstraints().add(rowConstraints);
 		add(button, 0, rowCounter++);
+		button.setOnAction(event -> controller.choosenCategory(subcategory.getKey()));
 	}
 	
 }
